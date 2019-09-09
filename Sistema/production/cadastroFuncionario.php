d17e2ef5c095b330457ebbc006a7603e43b26ca0
andré luiz benevenuto da silva <andre_luiz_7777@hotmail.com>
2019-09-09 16:06:11 -0300

    mascaras implementadas

--- Sistema/production/cadastroFuncionario.php
+++ Sistema/production/cadastroFuncionario.php
@@ -9,25 +9,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Sys'Odont</title>
-
     <!-- Bootstrap -->
     <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- NProgress -->
     <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
-    <!-- bootstrap-progressbar -->
-    <link href="../vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
     <!-- bootstrap-daterangepicker -->
     <link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <!-- bootstrap-datetimepicker -->
+    <link href="../vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+    <!-- Ion.RangeSlider -->
+    <link href="../vendors/normalize-css/normalize.css" rel="stylesheet">
+    <link href="../vendors/ion.rangeSlider/css/ion.rangeSlider.css" rel="stylesheet">
+    <link href="../vendors/ion.rangeSlider/css/ion.rangeSlider.skinFlat.css" rel="stylesheet">
+    <!-- Bootstrap Colorpicker -->
+    <link href="../vendors/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css" rel="stylesheet">
+
+    <link href="../vendors/cropper/dist/cropper.min.css" rel="stylesheet">
 
     <!-- Custom Theme Style -->
     <link href="../build/css/custom.min.css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/37b548cb8d.js"></script>
-
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
-
 
 </head>
 
@@ -116,7 +118,7 @@
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="pwd">CPF:<span class="required">*</span></label>
                         <div class="col-sm-2">
-                            <input type="text" class="form-control" id="CPF" name="cpf" required="required">
+                            <input type="text" class="form-control" data-inputmask="'mask' : '999.999.999-99'" id="CPF" name="cpf" required="required">
                         </div>
                         <div class="form-group">
                             <label class="control-label col-sm-2" for="DataNascimento">Data de nascimento:<span class="required">*</span></label>
@@ -138,13 +140,13 @@
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="RG">RG:<span class="required">*</span></label>
                         <div class="col-sm-2">
-                            <input type="text" class="form-control" required="required" id="RG" name="ds_rg">
+                            <input type="text" class="form-control" data-inputmask="'mask' : '99.999.999-99'" required="required" id="RG" name="ds_rg">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="PIS">PIS/PASEP:<span class="required">*</span></label>
                         <div class="col-sm-2">
-                            <input type="text" class="form-control" required="required" id="PIS" name="ds_pis">
+                            <input type="text" class="form-control" data-inputmask="'mask' : '999.99999.99-9'" required="required" id="PIS" name="ds_pis">
                         </div>
                     </div>
                     <div class="form-group">
@@ -155,8 +157,8 @@
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="pwd">CEP:<span class="required">*</span></label>
-                        <div class="col-sm-1">
-                            <input type="text" class="form-control" id="CEP" name="ds_cep">
+                        <div class="col-sm-2">
+                            <input type="text" class="form-control" data-inputmask="'mask' : '99999-999'" id="CEP" name="ds_cep">
                         </div>
                         <div class="form-group">
                             <label class="control-label col-sm-2" for="NumCasa">Número:<span class="required">*</span></label>
@@ -169,19 +171,19 @@
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="referencia">Ponto de Referência:</label>
                         <div class="col-sm-4">
-                            <input type="text" class="form-control" required="required" id="referencia">
+                            <input type="text" class="form-control" id="referencia">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="Telefone">Telefone:<span class="required">*</span></label>
                         <div class="col-sm-3">
-                            <input type="text" class="form-control" id="Telefone">
+                            <input type="text" class="form-control" data-inputmask="'mask' : '(99) 99999-9999'" id="Telefone" name="ds_tel">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="Email">Email:<span class="required">*</span></label>
                         <div class="col-sm-4">
-                            <input type="text" class="form-control" required="required" id="Email">
+                            <input type="email" class="form-control" required="required" id="Email">
                         </div>
                     </div>
                     <div class="form-group">
@@ -209,38 +211,57 @@
     <script src="../vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
     <script src="../vendors/nprogress/nprogress.js"></script>
-    <!-- Chart.js -->
-    <script src="../vendors/Chart.js/dist/Chart.min.js"></script>
-    <!-- jQuery Sparklines -->
-    <script src="../vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-    <!-- morris.js -->
-    <script src="../vendors/raphael/raphael.min.js"></script>
-    <script src="../vendors/morris.js/morris.min.js"></script>
-    <!-- gauge.js -->
-    <script src="../vendors/gauge.js/dist/gauge.min.js"></script>
-    <!-- bootstrap-progressbar -->
-    <script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
-    <!-- Skycons -->
-    <script src="../vendors/skycons/skycons.js"></script>
-    <!-- Flot -->
-    <script src="../vendors/Flot/jquery.flot.js"></script>
-    <script src="../vendors/Flot/jquery.flot.pie.js"></script>
-    <script src="../vendors/Flot/jquery.flot.time.js"></script>
-    <script src="../vendors/Flot/jquery.flot.stack.js"></script>
-    <script src="../vendors/Flot/jquery.flot.resize.js"></script>
-    <!-- Flot plugins -->
-    <script src="../vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-    <script src="../vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-    <script src="../vendors/flot.curvedlines/curvedLines.js"></script>
-    <!-- DateJS -->
-    <script src="../vendors/DateJS/build/date.js"></script>
     <!-- bootstrap-daterangepicker -->
     <script src="../vendors/moment/min/moment.min.js"></script>
     <script src="../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
-
+    <!-- bootstrap-datetimepicker -->
+    <script src="../vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+    <!-- Ion.RangeSlider -->
+    <script src="../vendors/ion.rangeSlider/js/ion.rangeSlider.min.js"></script>
+    <!-- Bootstrap Colorpicker -->
+    <script src="../vendors/mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js"></script>
+    <!-- jquery.inputmask -->
+    <script src="../vendors/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js"></script>
+    <!-- jQuery Knob -->
+    <script src="../vendors/jquery-knob/dist/jquery.knob.min.js"></script>
+    <!-- Cropper -->
+    <script src="../vendors/cropper/dist/cropper.min.js"></script>
+    <!-- validator -->
+    <script src="../vendors/validator/validator.js"></script>
     <!-- Custom Theme Scripts -->
     <script src="../build/js/custom.min.js"></script>
 
+    <!-- Initialize datetimepicker -->
+    <script>
+        $('#myDatepicker').datetimepicker();
+
+        $('#myDatepicker2').datetimepicker({
+            format: 'DD.MM.YYYY'
+        });
+
+        $('#myDatepicker3').datetimepicker({
+            format: 'hh:mm A'
+        });
+
+        $('#myDatepicker4').datetimepicker({
+            ignoreReadonly: true,
+            allowInputToggle: true
+        });
+
+        $('#datetimepicker6').datetimepicker();
+
+        $('#datetimepicker7').datetimepicker({
+            useCurrent: false
+        });
+
+        $("#datetimepicker6").on("dp.change", function(e) {
+            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+        });
+
+        $("#datetimepicker7").on("dp.change", function(e) {
+            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+        });
+    </script>
 </body>
 
 </html>
--- Sistema/production/index.php
+++ Sistema/production/index.php
@@ -99,7 +99,25 @@
 
       <!-- page content -->
       <div class="right_col" role="main">
-
+          <!-- top tiles -->
+          <div class="row tile_count">
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Pacientes Cadastrados</span>
+              <div class="count">0</div>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-clock-o"></i> Consultas Marcadas</span>
+              <div class="count">0</div>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-calendar"></i> Consultas Para Hoje</span>
+              <div class="count">0</div>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+              <span class="count_top"><i class="fa fa-calendar"></i> Consultas da Semana</span>
+              <div class="count">0</div>
+            </div>
+          </div>
       </div>
 
       <!-- footer content -->
