df8d306b25da9d106cf0cb3819b5b5b54357d8ff
linkzito <44907687+linkzito@users.noreply.github.com>
2019-09-09 09:08:45 -0300

    validação de login criado

---
+++ Sistema/valida_login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+//pegando os valores da telalogin 
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
+try {
+    require('conexao.php');    
+    
+    $stmt = $conn->prepare("select * from usuario where usua_nm_usuario = '$usuario' and usua_nm_senha = '$senha'");
+    $stmt->execute();
+    $linha = $stmt->fetchAll();
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+if (isset($linha[0])){
+    $_SESSION['logar']=$usuario;
+    header('Location: TelaPerfil.php');
+}else{
+    $_SESSION["msg_erro"]="Usuário ou senha inválidos.";
+    header('Location: TelaLogin.php');
+}
+?>
