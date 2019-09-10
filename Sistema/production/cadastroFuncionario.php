fe15c5735fcf0e08f6839ba1960c37501b1177ad
dteslab <talesfranca.97@gmail.com>
2019-09-10 15:20:29 -0300

    Subiu o que Matheus fez (Cadastro de Paciente)

--- Sistema/production/cadastroFuncionario.php
+++ Sistema/production/cadastroFuncionario.php
@@ -232,36 +232,6 @@
     <script src="../build/js/custom.min.js"></script>
 
     <!-- Initialize datetimepicker -->
-    <script>
-        $('#myDatepicker').datetimepicker();
-
-        $('#myDatepicker2').datetimepicker({
-            format: 'DD.MM.YYYY'
-        });
-
-        $('#myDatepicker3').datetimepicker({
-            format: 'hh:mm A'
-        });
-
-        $('#myDatepicker4').datetimepicker({
-            ignoreReadonly: true,
-            allowInputToggle: true
-        });
-
-        $('#datetimepicker6').datetimepicker();
-
-        $('#datetimepicker7').datetimepicker({
-            useCurrent: false
-        });
-
-        $("#datetimepicker6").on("dp.change", function(e) {
-            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-        });
-
-        $("#datetimepicker7").on("dp.change", function(e) {
-            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-        });
-    </script>
 </body>
 
 </html>
