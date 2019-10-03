320db133ffe9e3fde537db127f190e9316faa2c5
Gabriel-17 <gabrielr440@gmail.com>
2019-10-03 14:02:42 -0300

    Alteração do código do gráfico

--- Sistema/production/index.php
+++ Sistema/production/index.php
@@ -55,46 +55,39 @@
     <!-- top navigation -->
     <?php include("./View/topNavigation.php") ?>
     <!-- page content -->
-      <div class="right_col" role="main">
-        <div class="">
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="x_panel">
-              <div class="x_title">
-                <h2>Total Clientes<small>Mensal</small></h2>
-                <ul class="nav navbar-right panel_toolbox">
-                  <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                  </li>
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                    <ul class="dropdown-menu" role="menu">
-                      <li><a href="#">Settings 1</a>
-                      </li>
-                      <li><a href="#">Settings 2</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li><a class="close-link"><i class="fa fa-close"></i></a>
-                  </li>
-                </ul>
-                <div class="clearfix"></div>
-              </div>
-              <div class="x_content">
-                <canvas id="mybarChart"></canvas>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- /page content -->
-      <!-- footer content -->
-      <footer>
-        <div class="pull-right">
-          Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-        </div>
-        <div class="clearfix"></div>
-      </footer>
-      <!-- /footer content -->
+    <div class="chart-container" style="position: relative; height:20vh; width:40vw">
+      <canvas class="bar-chart"></canvas>
+      <canvas id="myChart"></canvas>
+
+      <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+      <script>
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            datasets: [{
+              label: 'Controle Mensal de clientes',
+              backgroundColor: '#2A3F54',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [50, 10, 5, 2, 20, 30, 45, 100, 10, 5, 30, 20]
+            }]
+          },
+
+          options: {}
+        });
+      </script>
     </div>
+    <!-- /page content -->
+    <!-- footer content -->
+    <footer>
+      <div class="pull-right">
+        Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+      </div>
+      <div class="clearfix"></div>
+    </footer>
+    <!-- /footer content -->
+  </div>
   </div>
 
   <!-- jQuery -->
