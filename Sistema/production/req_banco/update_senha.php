5f4e6f330375326c446362b02a51f606322869ac
dteslab <talesfranca.97@gmail.com>
2019-11-02 12:28:06 -0300

    update senha

---
+++ Sistema/production/req_banco/update_senha.php
@@ -0,0 +1,20 @@
+<?php
+try {
+    session_start();
+    require('conexao.php');
+    //Alteração da senha
+    echo ($_POST['pw']);
+
+    $stmt = $conn->prepare('UPDATE senha SET usua_nm_senha = :usua_nm_senha WHERE usua_nm_senha = 
+    "' . $_POST['pw'] . '"');
+    $stmt->execute(array(
+        ':usua_nm_senha' => $_POST['pw'],
+    ));
+    echo $stmt->rowCount();
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+if (isset($stmt)) {
+    $_SESSION['editado'] = 1;
+    header('Location: ../mod_senha.php');
+}
