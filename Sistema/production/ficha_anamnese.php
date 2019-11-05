afa4ab3d25d98d22661e7942bf2101af5eabcb0e
Gabriel-17 <gabrielr440@gmail.com>
2019-11-05 11:45:11 -0300

    Aplicação FrontEnd criado por Matheus em uma nova página

--- Sistema/production/ficha_anamnese.php
+++ Sistema/production/ficha_anamnese.php
@@ -39,8 +39,7 @@
     <script src="https://kit.fontawesome.com/37b548cb8d.js"></script>
 
 </head>
-
- <?php include("./View/verificar_Login.php") ?>
+<?php include("./View/verificar_Login.php") ?>
 
 <body class="nav-md">
     <div class="container body">
@@ -72,101 +71,137 @@
                         <h2>Ficha Anamnese</h2>
                         <div class="clearfix"></div>
                     </div>
+                    <form class="form-horizontal" method="" action="">
 
-                    <form class="form-horizontal" method=" " action=" ">
                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="">Nome Completo:<span class="required">*</span></label>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" required="required" id="" name="">
+                            <div>
+                                <label for="">Se faz uso de algum medicamento informe abaixo:</label>
+                                <p><input name="in_medicamento" placeholder="Digite aqui..." class="form-control"></p>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="">Faz uso medicamento, Quais?<span class="required">*</span></label>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" required="required" id="" name="">
+                            <label for="">Se o paciente tiver algum problema de saúde informe
+                                abaixo:</label>
+                            <div>
+                                <p><input name="in_problema" placeholder="Digite aqui..." class="form-control"></p>
                             </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="">Faz uso medicamento, Quais?<span class="required">*</span></label>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" required="required" id="" name="">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Se o paciente estiver em algum tratamento ou acompanhamento
+                                informe
+                                abaixo:</label>
+                            <div>
+                                <p><input name="in_acompanhamento" placeholder="Digite aqui..." class="form-control"></p>
                             </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="">Problemas de saúde, Quais?<span class="required">*</span></label>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" required="required" id="" name="">
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="">Faz acompanhamento?<span class="required">*</span></label>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" required="required" id="" name="">
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="">Alérgico(a) a algum medicmento?<span class="required">*</span></label>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" required="required" id="" name="">
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="">Existe de casos de hemorragia<span class="required">*</span></label>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" required="required" id="" name="">
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="">Hábitos do dia a dia, sedentário?<span class="required">*</span></label>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" required="required" id="" name="">
-                            </div>
-                         </div>
-                         <div class="form-group">
-                                <label class="control-label col-sm-2" for="">Fumante?<span class="required">*</span></label>
-                                <div class="col-sm-2">
-                                    <select class="form-control" id="" name="">
-                                        <option value=""> </option>
-                                        <option value="">NÃO</option>
-                                        <option value="">SIM</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-sm-2" for=""> Faz uso de bebidas alcoólicas?<span class="required">*</span></label>
-                                <div class="col-sm-2">
-                                    <select class="form-control" id="" name="">
-                                        <option value=""> </option>
-                                        <option value="">NÃO</option>
-                                        <option value="">SIM, mais de uma vez por semana</option>
-                                        <option value="">SIM, de uma a duas vezes no mês</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="col-sm-offset-2 col-sm-10">
-                                    <button type="submit" class="btn estilo" id="cadastrar"style="margin-top:30px">Cadastrar</button>
-                                </div>
-                            </div>
-                       
-                
-                
-                
-                
-                
-                
+                        </div>
 
-                            
+                        <div class="form-group">
+                            <label for="">Se o paciente tiver alergia a algum medicamento informe
+                                abaixo:</label>
+                            <div>
+                                <p><input name="in_alergia" placeholder="Digite aqui..." class="form-control"></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Se o paciente já teve hemorragia
+                                alguma vez ou
+                                foi internado alguma vez informe
+                                abaixo o motivo:</label>
+                            <div>
+                                <p><input name="in_hemorragia" placeholder="Digite aqui..." class="form-control"></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Se o paciente fuma ou já fumou
+                                alguma vez informe por quanto tempo:</label>
+                            <div>
+                                <p><input name="in_fumante" placeholder="Digite aqui..." class="form-control"></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="">O paciente possui alguns hábitos como
+                                apertamento, bruxismo, morder objetos?</label>
+                            <div>
+                                <p><input name="in_habitos" placeholder="Digite aqui..." class="form-control"></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea3">Como faz a higiene oral? Quantas vezes
+                                ao dia? Usa fio dental ou algum bochecho com flúor?(Responda na
+                                ordem)</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea3" rows="5"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea3">Já foi ao dentista antes? Quando foi
+                                seu último tratamento?(Responda na ordem)</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea3" rows="5"></textarea>
+                        </div>
+                        <h3>Crianças</h3>
+                        <div class="form-group">
+                            <label for="">Quem faz a higieno bucal da criança?</label>
+                            <div>
+                                <p><input placeholder="Digite aqui..." oninput="this.className = ''" class="form-control"></p>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea3">Defina a alimentação básica da
+                                criança:</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea3" rows="5"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea3">Defina a alimentação básica da
+                                criança:</label>
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="col-md-2">Faz uso de chupeta ou mamadeira?</label>
+                            <div class="form-check form-check-inline">
+                                <div class="col-sm-1">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+
+                                    <label class="form-check-label" for="inlineRadio1">Sim</label>
+                                </div>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <div class="col-sm-1">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                    <label class="form-check-label" for="inlineRadio1">Não</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <p>Pressão Arterial</p>
+                            <p><input class="form-control col-md-2"></p>
+                            <br>
+                            <br>
+                            <p>
+                                <input type="text" class="form-control col-md-2" required="required" id="DataPA" name="dt_pa" data-inputmask="'mask': '99/99/9999'" placeholder="Data da registro">
+                            </p>
+                            <br>
+                            <br>
+                            <p>Batimentos por minuto</p>
+                            <p><input class="form-control col-md-2"></p>
+                            <br>
+                            <br>
+                            <p>
+                                <input type="text" class="form-control col-md-2" required="required" id="DataBPM" name="dt_bpm" data-inputmask="'mask': '99/99/9999'" placeholder="Data de registro">
+                            </p>
+                        </div>
+
+                        <br>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn estilo" style="margin-top: 30px">Cadastrar</button>
                             </div>
                         </div>
                 </div>
-                </form>
             </div>
-            <!-- footer content -->
-            <?php include("./View/footer.php") ?>
+            </form>
         </div>
+        <!-- footer content -->
+        <?php include("./View/footer.php") ?>
     </div>
-    
+    </div>
 
     <!-- jQuery -->
     <script src="../vendors/jquery/dist/jquery.min.js"></script>
@@ -197,14 +232,6 @@
     <script src="../build/js/custom.min.js"></script>
 
     <!-- Initialize datetimepicker -->
-    <script>
-    function FunctionConvenio(item) {
-    if (item == 1)
-        $('.convenios').removeClass('hidden');
-    else
-        $('.convenios').addClass('hidden')
-}
-    </script>
 </body>
 
 </html>
